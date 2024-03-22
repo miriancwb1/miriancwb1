a641b76397091b4ea1a491973d0e0e3ca019ae94
miriancwb1 <163296336+miriancwb1@users.noreply.github.com>
2024-03-22 08:37:03 -0300

    Add files via upload

---
+++ codificaçãoDeCadastro.py
@@ -0,0 +1,15 @@
+
+nome = str( input("digite o nome"))
+
+telefone = str(input ("digite o número de telefone"))
+
+email = str (input ( " digite o e-mail"))
+
+indereco = str ( input ("digite o indereço"))
+
+idade = int (input ("digite a idade"))
+
+sexo = str ( input  ("digite o sexo"))
+
+salario = float ( input  ("digite o valor do salário"))
+
